56063e5e5eab928694c4360914af886ce636f785
Buck Randall <tbr446@gmail.com>
2022-03-12 00:49:23 -0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
-function App() {
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components'
+import './App.css'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route path='/' element={<Homepage/>} />
+              
+              <Route path='/homepage' element={<Homepage/>} />
+
+              <Route path='/exchanges' element={<Exchanges/>}/>
+              
+              <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                
+              <Route path='/crypto/:coinID' element={<CryptoDetails />} />
+              
+              <Route path='/news' element={<News />} />
+
+            </Routes>
+          </div>
+
+        </Layout>
+      
+
+      <div className='footer' >
+        <Typography.Title level={5} style={{color: 'white', textAlign: 'center' }}>
+          CryptoCentral <br />
+          All Rights Reserved
+        </Typography.Title>
+        <Space >
+            <Link to='/homepage' className='links'>Home</Link>
+            <Link to='/exchanges' className='links'>Exchanges</Link>
+            <Link to='/news' className='links'>News</Link>
+        </Space>
+        
+      </div>
+      </div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'b0f1366c80mshdb9f045be44ee57p1784a8jsnea1041e2bce0'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        })
+    })
+
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
